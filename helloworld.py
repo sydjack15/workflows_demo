aeb738a4782d1d2a97e867b9b3817b33ffcc435a
sydjack15 <sydjackson.ai@gmail.com>
2025-09-20 21:22:58 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,6 @@
-import sys
-
 def main():
-    print("Hello World! From Python: " + str(sys.version_info))
-    if sys.version_info >= (3, 7) and sys.version_info < (3, 8):
-        # let's make this script fail for Python 3.7
-        raise Exception('Python version 3.7.x is unsupported!')
+    print("Hello World!")
+
 
 if __name__ == "__main__":
     main()
